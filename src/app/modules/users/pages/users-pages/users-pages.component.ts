b0c49ae2fd1e39ff8d1269a6782d58bc00c2f01b
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2025-02-28 09:43:46 -0500

    Add new user interfaces, registration modal components, and update user pages and services

--- src/app/modules/users/pages/users-pages/users-pages.component.ts
+++ src/app/modules/users/pages/users-pages/users-pages.component.ts
@@ -1,11 +1,161 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2'
+
+//angular material
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginator} from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {DialogModule} from '@angular/cdk/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { CdkTableModule } from '@angular/cdk/table';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
+//interfaces
+import { Usuario } from '../../../../core/interface/usuario.interface';
+import { UsuarioService } from '../../services/usuario.service';
+import { UtilidadService } from '../../../../services/utilidad.service';
+
+//componete modoal
+import { ModalRegistroComponent } from '../../components/modal-registro/modal-registro.component';
+
 
 @Component({
   selector: 'app-users-pages',
-  imports: [],
+  imports: [
+    MatTableModule,
+    CdkTableModule,
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    DialogModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSlideToggleModule
+  ],
   templateUrl: './users-pages.component.html',
   styleUrl: './users-pages.component.scss'
 })
-export class UsersPagesComponent {
+export class UsersPagesComponent implements OnInit, AfterViewInit {
+  columnasTabla: string[] = ['nombreCompleto', 'correo', 'rolDescription', 'estado', 'acciones'];
+  dataInicio: Usuario[] = []
+  datalistaUsuario = new MatTableDataSource(this.dataInicio)
+  @ViewChild(MatPaginator) paginacionTabla!: MatPaginator
+
+  private dialog = inject(MatDialog)
+  private usuarioService = inject(UsuarioService)
+  private utiliadService = inject(UtilidadService)
+
+  obtenerUsuarios(){
+    this.usuarioService.lista()
+    .subscribe({
+      next: (data) => {
+        if(data.status)
+          this.datalistaUsuario.data = data.value;
+        else
+        this.utiliadService.mostrarAlerta("No se encontraron datos","Ooops!")
+      }
+    })
+  }
+
+
+  ngOnInit(): void {
+    this.obtenerUsuarios();
+  }
+
+  //CREAMOS LA PAGINACION CON ESTE EVENTO (ngAfterViewInit)
+  ngAfterViewInit(): void {
+    this.datalistaUsuario.paginator = this.paginacionTabla
+  }
+
+  //METODO PARA FILTRAR
+  aplicarFiltroTabla(event: Event){
+    const filterValue = (event.target as HTMLInputElement).value
+    this.datalistaUsuario.filter = filterValue.trim().toLocaleLowerCase()
+  }
+
+  //METODO PARA EL MODAL DE CREAR USUARIO
+  nuevoUsuario(){
+    this.dialog.open( ModalRegistroComponent,{
+      disableClose:true,
+    }).afterClosed()
+    .subscribe({
+      next: (resultado) => {
+        if(resultado === 'true')this.obtenerUsuarios()
+      }
+    })
+  }
+
+  //METODO PARA EL MODAL DE ACTUALIZAR USUARIO
+  editarUsuario(usuario: Usuario){
+    this.dialog.open( ModalRegistroComponent,{
+      disableClose:true,
+      data: usuario
+    }).afterClosed()
+    .subscribe({
+      next:(resultado) => {
+        if(resultado === 'true')this.obtenerUsuarios();
+      },
+    })
+  }
+
+  //METODO PARA CAMBIAR EL ESTADO DEL USURIO
+  cambiarEstado(usuario: Usuario) {
+    const nuevoEstado = usuario.esActivo === 1 ? 0 : 1;
+    this.usuarioService.actualizarEstado(usuario.idUsuario, nuevoEstado)
+      .subscribe({
+        next: (data) => {
+          if (data.status) {
+            this.utiliadService.mostrarAlerta("Estado actualizado", "Listo!");
+            this.obtenerUsuarios();
+          } else {
+            this.utiliadService.mostrarAlerta("No se pudo actualizar el estado", "Error");
+          }
+        },
+        error: (e) => {
+          this.utiliadService.mostrarAlerta("Error al actualizar el estado", "Error");
+        }
+      });
+  }
+
+  //METODO PARA ELIMINAR USUARIO
+  eliminarUsuario(usuario: Usuario){
+     //libreria de alertas personalizadas
+     Swal.fire({
+      title:'¿Desea eliminar el usuario',
+      text: usuario.nombreCompleto,
+      icon:'warning',
+      confirmButtonColor:'#3085d6',
+      confirmButtonText:'Si, eliminar',
+      showCancelButton:true,
+      cancelButtonColor: '#d33',
+      cancelButtonText:'No, volver'
+    }).then((resultado) => {
+
+      if (resultado.isConfirmed) {
+        this.usuarioService.eliminar(usuario.idUsuario)
+        .subscribe({
+          next: (data) => {
+            if (data.status) {
+              this.utiliadService.mostrarAlerta("El usuario fue eliminado","Listo!")
+              this.obtenerUsuarios();
+            } else
+              this.utiliadService.mostrarAlerta("No se pudo eliminar el usuario","Error")
+          },
+          error:(e) => {}
+        })
+      }
+    })
+  }
 
 }
---
+++ src/app/modules/users/services/rol.service.ts
@@ -0,0 +1,21 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ResponseApi } from '../../../core/interface/response.-api';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RolService {
+
+  private urlApi:string = environment.API_URL + "Rol/"
+
+  private http = inject(HttpClient)
+
+  constructor() { }
+
+  lista():Observable<ResponseApi>{
+    return this.http.get<ResponseApi>(`${this.urlApi}Lista`)
+  }
+}
