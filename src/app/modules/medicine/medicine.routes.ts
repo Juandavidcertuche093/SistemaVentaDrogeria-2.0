3c57e8b3f82c2dc772eaf133f7d09422c6cfdfd2
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2025-02-27 19:26:27 -0500

    Add initial medicine module and pages components

---
+++ src/app/modules/medicine/medicine.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { MedicinePageComponent } from "./pages/medicine-page/medicine-page.component";
+
+export const medicieneRoutes: Routes = [
+  {
+    path:'',
+    component:MedicinePageComponent
+  }
+]
---
+++ src/app/modules/medicine/pages/medicine-page/medicine-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medicine-page',
+  imports: [],
+  templateUrl: './medicine-page.component.html',
+  styleUrl: './medicine-page.component.scss'
+})
+export class MedicinePageComponent {
+
+}
